8b77591a690fbeff0fabd23729337ea7a921afc0
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-29 21:55:48 -0500

    Added getEmailWithCertification method in StudentsServices.js and create its test

--- test/services/StudentsServices.test.js
+++ test/services/StudentsServices.test.js
@@ -5,6 +5,11 @@ describe("Test for StudentsServices", () => {
     test("1) Return a list of students with all their fields", () => {
         const students = Reader.readJsonFile("test/data/visualpartners.test.json")
         const result = StudentsServices.getAllStudents(students)
+        expect(result.length).toBe(3)
+    })
+    test("2) Return a list of emails with certification", () => {
+        const students = Reader.readJsonFile("test/data/visualpartners.test.json")
+        const result = StudentsServices.getAllEmailsWithCertification(students)
         expect(result.length).toBe(2)
     })
 })
