4452650e548a60302065897b014f179fde0a9023
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-29 22:21:34 -0500

    Added getAllStudentsWithCreditsGreaterThan method in StudentServices and create its test

--- test/services/StudentsServices.test.js
+++ test/services/StudentsServices.test.js
@@ -12,4 +12,9 @@ describe("Test for StudentsServices", () => {
         const result = StudentsServices.getAllEmailsWithCertification(students)
         expect(result.length).toBe(2)
     })
+    test("3) Return a list of students when their credits are greater than credit of the params ", () => {
+        const students = Reader.readJsonFile("test/data/visualpartners.test.json")
+        const result = StudentsServices.getAllStudentsWithCreditsGreaterThan(students, 500)
+        expect(result.length).toBe(2)
+    })
 })
